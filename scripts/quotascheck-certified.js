fc8ecb14556babb062dea348c04e5a9e6ab01298
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-10-05 11:52:05 +0300

    Update quotascheck-certified.js

--- scripts/quotascheck-certified.js
+++ scripts/quotascheck-certified.js
@@ -7,24 +7,24 @@ var cloudlets = 16;
 
 var max = 10, min = 2, resp, name, value, ssl, markup = "", ssl_markup = "";
 
-  var hasCollaboration = (parseInt('${fn.compareEngine(7.0)}', 10) >= 0),
-    q = [];
+var hasCollaboration = (parseInt('${fn.compareEngine(7.0)}', 10) >= 0),
+  q = [];
       
-  if (hasCollaboration) {
-  q = [
-        { quota : { name: MAX_CLOUDLET }, value: parseInt('${quota.environment.maxcloudletsperrec}', 10) },
-        { quota : { name: SAME_NODES }, value: parseInt('${quota.environment.maxsamenodescountt}', 10) },
-        { quota : { name: MAX_NODES }, value: parseInt('${quota.environment.maxnodescount}', 10) },
-        { quota : { name: SSL }, value: parseInt('${quota.environment.jelasticssl.enabled}', 10) }
-    ];
-        group = { groupType: '${account.groupType}' };
-    } else {
-        q.push(jelastic.billing.account.GetQuotas(MAX_CLOUDLET).array[0]);
-        q.push(jelastic.billing.account.GetQuotas(SAME_NODES).array[0]);
-        q.push(jelastic.billing.account.GetQuotas(MAX_NODES).array[0]);
-        q.push(jelastic.billing.account.GetQuotas(SSL).array[0]);
-        group = jelastic.billing.account.GetAccount(appid, session);
-  }
+if (hasCollaboration) {
+q = [
+      { quota : { name: MAX_CLOUDLET }, value: parseInt('${quota.environment.maxcloudletsperrec}', 10) },
+      { quota : { name: SAME_NODES }, value: parseInt('${quota.environment.maxsamenodescountt}', 10) },
+      { quota : { name: MAX_NODES }, value: parseInt('${quota.environment.maxnodescount}', 10) },
+      { quota : { name: SSL }, value: parseInt('${quota.environment.jelasticssl.enabled}', 10) }
+  ];
+      group = { groupType: '${account.groupType}' };
+  } else {
+      q.push(jelastic.billing.account.GetQuotas(MAX_CLOUDLET).array[0]);
+      q.push(jelastic.billing.account.GetQuotas(SAME_NODES).array[0]);
+      q.push(jelastic.billing.account.GetQuotas(MAX_NODES).array[0]);
+      q.push(jelastic.billing.account.GetQuotas(SSL).array[0]);
+      group = jelastic.billing.account.GetAccount(appid, session);
+}
 
 for (var i = 0, n = q.length; i < n; i++) {
   name = q[i].quota.name;
